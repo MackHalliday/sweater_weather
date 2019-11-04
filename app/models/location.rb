46003ffc4c390cbbab8a33d0e4c5130525b38f3b
MackHalliday <mackenzie.halliday@gmail.com>
2019-11-04 11:20:57 -0700

    Add attr_reader for attributes

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,16 +1,16 @@
 class Location
-  attr_reader :formated_address, :latitude, :longitude
+  attr_reader :formatted_address, :latitude, :longitude
   def initialize(geocode_data)
     @latitude = geocode_data[:results][0][:geometry][:location][:lat]
     @longitude = geocode_data[:results][0][:geometry][:location][:lng]
-    @formated_address = geocode_data[:results][0][:formatted_address]
+    @formatted_address = geocode_data[:results][0][:formatted_address]
     @city = city
     @state = state
     @country = country
   end
 
   def parse_location
-    @formated_address.split(',')
+    @formatted_address.split(',')
   end
 
   def city
