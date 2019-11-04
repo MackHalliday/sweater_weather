dfa0ad5debefbf982b1642d2b9d7256ea6340a8e
MackHalliday <mackenzie.halliday@gmail.com>
2019-11-04 11:34:27 -0700

    Test reverse look up for geocode service

--- spec/services/google_geocity_service_spec.rb
+++ spec/services/google_geocity_service_spec.rb
@@ -12,4 +12,16 @@ describe 'google geocode service' do
     expect(raw_data[:results][0][:geometry][:location][:lat]).to eq(39.7392358)
     expect(raw_data[:results][0][:geometry][:location][:lng]).to eq(-104.990251)
   end
+
+  it 'can lookup address by latitude and longitude', :vcr do
+    service = GoogleGeocodeService.new
+    latitude = -22.3193039
+    longitude = -65.8306389
+
+    raw_data = service.reverse_geocode(latitude, longitude)
+
+    expect(service).to be_a(GoogleGeocodeService)
+    expect(raw_data).to be_a(Hash)
+    expect(raw_data[:results][0][:formatted_address]).to eq("Jujuy, Argentina")
+  end
 end
