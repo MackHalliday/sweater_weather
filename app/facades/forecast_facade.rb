c8862a471a51819ffb85bc3908ab36bbedba42b4
MackHalliday <mackenzie.halliday@gmail.com>
2019-11-05 16:13:39 -0700

    Add memoization

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -4,15 +4,15 @@ class ForecastFacade
   end
 
   def geocode_data
-    GoogleGeocodeService.new.get_geocode_data(@location)
+    @geocode_date ||= GoogleGeocodeService.new.get_geocode_data(@location)
   end
 
   def location_info
-    Location.new(geocode_data)
+    @location_info ||= Location.new(geocode_data)
   end
 
   def dark_sky_data
-    DarkSkyService.new.get_weather(location_info.latitude, location_info.longitude)
+    @dark_sky_data ||= DarkSkyService.new.get_weather(location_info.latitude, location_info.longitude)
   end
 
   def current_data
@@ -20,29 +20,26 @@ class ForecastFacade
   end
 
   def daily_data
+
     dark_sky_data[:daily][:data]
   end
 
   def current_weather
-    CurrentWeather.new(current_data)
+    @current_weather ||= CurrentWeather.new(current_data)
   end
 
   def daily_weather
+
     daily_data.map.each_with_index do |data, index|
       DailyWeather.new(data, index + 1)
     end
   end
 
   def summary
-    Summary.new(dark_sky_data)
+    @summary ||= Summary.new(dark_sky_data)
   end
 
   def data
-    { id: 1,
-      location: location_info,
-      summary: summary,
-      current_weather: current_weather,
-      daily_weather: daily_weather,
-    }
+    ForecastData.new(summary, location_info, current_weather, daily_weather)
   end
 end
