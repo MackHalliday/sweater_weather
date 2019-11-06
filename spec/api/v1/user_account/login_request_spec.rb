5099cfa35d22e71792626ef7ce40b0df1b2067df
MackHalliday <mackenzie.halliday@gmail.com>
2019-11-05 22:12:30 -0700

    Add sad path testing for invalid email or password during login

--- spec/api/v1/user_account/login_request_spec.rb
+++ spec/api/v1/user_account/login_request_spec.rb
@@ -17,4 +17,40 @@ describe "login endpoint" do
 
     expect(response.body).to eq(expected_response.to_json)
   end
+
+  it "cannot login user with wrong email" do
+
+    user = User.create!(email: "whatever@example.com", password: "password", token: "123")
+
+    request_body = {
+                    "email": "wrong_email@example.com",
+                    "password": "password",
+                    }
+
+    post "/api/v1/sessions", as: :json, params: request_body
+
+    expect(response).to have_http_status(400)
+
+    expected = {error: "Invalid email or password."}
+
+    expect(response.body).to eq(expected.to_json)
+  end
+
+  it "cannot login user with wrong password" do
+
+    user = User.create!(email: "whatever@example.com", password: "password", token: "123")
+
+    request_body = {
+                    "email": "whatever@example.com",
+                    "password": "wrong_password",
+                    }
+
+    post "/api/v1/sessions", as: :json, params: request_body
+
+    expect(response).to have_http_status(400)
+
+    expected = {error: "Invalid email or password."}
+
+    expect(response.body).to eq(expected.to_json)
+  end
 end
