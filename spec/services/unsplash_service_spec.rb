d1f9d8cc73ab03252511ce5607af74fff60e15de
MackHalliday <mackenzie.halliday@gmail.com>
2019-11-05 19:00:46 -0700

    Removed weather variable

--- spec/services/unsplash_service_spec.rb
+++ spec/services/unsplash_service_spec.rb
@@ -4,9 +4,8 @@ describe 'unsplash service' do
   it 'can return a background for a city', :vcr do
     service = UnsplashService.new
     location = "denver,co"
-    weather  = "sunny"
 
-    raw_data = service.get_background(location, weather)
+    raw_data = service.get_background(location)
 
     expect(service).to be_a(UnsplashService)
     expect(raw_data).to be_a(Hash)
