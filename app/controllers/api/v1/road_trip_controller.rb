5589585d83434bd868cca75b23de4cffb33269d3
MackHalliday <mackenzie.halliday@gmail.com>
2019-11-06 12:54:41 -0700

    Correct params for road trip

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -3,10 +3,15 @@ class Api::V1::RoadTripController < ApplicationController
   def show
     user = User.find_by(token: params[:api_key])
     if user.present?
-      facade = RoadTripFacade.new(params[:road_trip])
+      facade = RoadTripFacade.new(road_trip_params)
       render json: RoadTripSerializer.new(facade)
     else
       render json: {error: "Invalid request."}, status: 401
     end
+  end
+
+  private
+  def road_trip_params
+    params.permit(:api_key, :origin, :destination)
   end
 end
