fed0622170197a259d9f39ae40cf7a1dc8f34643
MackHalliday <mackenzie.halliday@gmail.com>
2019-11-05 21:29:18 -0700

    moved create_token method to create_after method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
 
-    validates_presence_of :password_digest
-    validates :email, presence: true,
-    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i },
-    uniqueness: { case_sensitive: false }
-    validates :password, confirmation: true
+  validates_presence_of :password_digest
+  validates :email, presence: true,
+  format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i },
+  uniqueness: { case_sensitive: false }
+  validates :password, confirmation: true
+  after_create :create_token
+
+  def create_token
+    self.token = SecureRandom.hex
+  end
 end
