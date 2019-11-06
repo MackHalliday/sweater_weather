05a389b6da270480ffe32f7546bd87046c7180cb
MackHalliday <mackenzie.halliday@gmail.com>
2019-11-05 18:57:21 -0700

    Add attributes for background image

--- app/facades/backgrounds_facade.rb
+++ app/facades/backgrounds_facade.rb
@@ -3,8 +3,19 @@ class BackgroundsFacade
     @location = location
   end
 
+  def geocode_data
+    @geocode_date ||= GoogleGeocodeService.new.get_geocode_data(@location)
+  end
+
+  def location_info
+    @location_info ||= Location.new(geocode_data)
+  end
+
+  def unsplash_data
+    @dark_sky_data ||= UnsplashService.new.get_background(location_info.formatted_address)
+  end
 
   def data
-    BackgroundsData.new(image_data)
+    BackgroundsData.new(unsplash_data)
   end
 end
